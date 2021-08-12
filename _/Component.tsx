09b0bdb34f539c0fa9a765419c195f8c53d71d7f
Jessé Jonas <jessejonas13@gmail.com>
2021-08-12 17:46:20 -0300

    feat: added default component

---
+++ _/Component.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { Box, Text } from '@chakra-ui/react';
+
+const EDIT_HERE: FC = () => {
+  return (
+    <Box>
+      <Text>EDIT_HERE</Text>
+    </Box>
+  );
+};
+
+export default EDIT_HERE;
