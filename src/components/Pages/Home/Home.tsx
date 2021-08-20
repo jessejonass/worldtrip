a17323bfe5a89847c079bb2df53c2bd1c441832c
Jesse Jonas <jessejonas13@gmail.com>
2021-08-20 08:55:32 -0300

    feat: added info container and divider

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -1,12 +1,16 @@
 import { FC } from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Divider } from '@chakra-ui/react';
 
 import Banner from 'components/Banner';
+import Info from 'components/Info';
 
 const Home: FC = () => {
   return (
     <Box w="100vw">
       <Banner />
+      <Info />
+
+      <Divider mb="50" borderWidth="1px" borderColor="text.900" maxW="90" mx="auto" />
     </Box>
   );
 };
