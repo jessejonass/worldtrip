c108181d451dc1becdd0b82e50dfa9d1aa3ba0e7
Jesse Jonas <jessejonas13@gmail.com>
2021-08-26 16:21:00 -0300

    feat: added hasBack propertie to header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,29 @@
 import { FC } from "react";
-import { Flex, Image, Link } from "@chakra-ui/react";
+import { Flex, Image, Link, Button, Icon } from "@chakra-ui/react";
+import { FiChevronLeft } from "react-icons/fi";
 
-const Header: FC = () => {
+import { HeaderProps } from "./types";
+
+const Header: FC<HeaderProps> = ({ hasBackButton = true }) => {
   return (
-    <Flex as="header" w="100%" align="center" justify="center" paddingY="6">
+    <Flex
+      as="header"
+      w="100%"
+      align="center"
+      justify="space-evenly"
+      paddingY="6"
+    >
+      {hasBackButton ? (
+        <Button borderRadius="50%" variant="ghost" h="10" w="8">
+          <Icon as={FiChevronLeft} h="32px" w="32px" color="tex.900" />
+        </Button>
+      ) : (
+        <div />
+      )}
       <Link href="/">
         <Image src="/images/logo.svg" h="45" />
       </Link>
+      <div />
     </Flex>
   );
 };
---
+++ src/components/Header/types.ts
@@ -0,0 +1,3 @@
+export type HeaderProps = {
+  hasBackButton: boolean;
+};
